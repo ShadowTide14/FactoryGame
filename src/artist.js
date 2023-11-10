8be09205a2146686c5e69feb38fbc8c4236735a8
ShadowTide14 <80174381+ShadowTide14@users.noreply.github.com>
2023-11-10 14:03:42 -0500

    changed background grid rendering

--- src/artist.js
+++ src/artist.js
@@ -7,16 +7,19 @@ canvasH = canvas.clientHeight;
 
 
 function drawGrid(camX_, camY_, camZoom_) {
-    for (let x = 0; x < Math.ceil(canvasW / (tileW * camZoom_)); x++) {
-        for (let y = 0; y < Math.ceil(canvasH / (tileW * camZoom_)); y++) {
+    for (let x = Math.floor(-camX_/tileW); x < 1 + Math.floor(-camX_/tileW) + Math.ceil(canvasW / (tileW * camZoom_)); x++) {
+        for (let y = Math.floor(-camY_/tileW); y < 1 + Math.floor(-camY_/tileW) + Math.ceil(canvasH / (tileW * camZoom_)); y++) {
             drawTile(x * tileW + camX_, y * tileW + camY_, tileW);
         }
     }
 }
 
 function drawTile(x, y, w, h=w) {
-    fill(235, 235, 235);
-    stroke(155, 155, 155);
+    let xdif = mouseX - (x + w/2);
+    let ydif = mouseY - (y + h/2);
+    let dist = Math.sqrt(xdif*xdif + ydif*ydif);
+    //fill(235 * (dist / (w*2)), 235 * (dist / (w*2)), 235 * (dist / (w*2)));
+    stroke(155, 155, 155,(w*w/(Math.pow(dist,2)+w*w)));
     strokeW(4);
     ctx.strokeRect(x, y, w, h);
 }
